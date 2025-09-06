8bb40a83c52b7c6c06ab502d6645dfb9533aaeac
GabrielSteph <gabrielsagerl@gmail.com>
2025-09-06 23:40:47 +0200

    A timer-app that counts down the time for a given timeframe. Very helpful for keeping track of tasks and time.

---
+++ pomodoro.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+import time
+
+a = int(input("How many minutes do you want to learn?"))*60
+runtime = a
+for i in range(0,a):
+
+    if runtime%60 < 10:
+        seconds = str(0)+str(runtime%60)
+    else:
+        seconds=runtime%60
+
+    print(f"{runtime//60} : {seconds}                                \r",end="")
+    time.sleep(1)
+    runtime = runtime - 1
+
+print("Congrats. You learned for", a//60, "minutes.")
