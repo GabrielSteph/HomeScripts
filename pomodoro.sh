a974656c2451a10ab2affb5a97e21424f5202af2
Gabriel Sagerl <gabrielsagerl@gmail.com>
2025-09-06 23:27:14 +0200

    Anpassungen zur Funktionalität

--- pomodoro.sh
+++ pomodoro.sh
@@ -2,6 +2,8 @@ i=0
 while [ $i -lt $1 ] ; do
 	i=$(($i+1));
 	sleep 60;
-	echo -n "$i Minuten hast du schon gelernt."; echo -ne "\r";
+	echo -ne "\r                                                   "; 
+	echo -n "$i Minuten hast du schon gelernt."; 
+	echo -ne "\r";
 done
-echo ""
+echo "Gratuliere! Dein Pomodoro ist zu Ende! Du hast $1 Minuten gelernt und gearbeitet."
