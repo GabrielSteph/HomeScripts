b7590e9553f8ea8fca4929f61f7b91a604c33fbb
GabrielSteph <gabrielsagerl@gmail.com>
2025-10-27 21:27:05 +0100

    removed the shellscript for pomodoro as the python script offered more functionality

--- pomodoro.sh
+++
@@ -1,9 +0,0 @@
-i=0
-while [ $i -lt $1 ] ; do
-	i=$(($i+1));
-	sleep 60;
-	echo -ne "\r                                                   "; 
-	echo -n "$i Minuten hast du schon gelernt."; 
-	echo -ne "\r";
-done
-echo "Gratuliere! Dein Pomodoro ist zu Ende! Du hast $1 Minuten gelernt und gearbeitet."
